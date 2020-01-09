329fa1d59c63d58da0976b6bef8e6dbeb75bc3a6
Cool_Guy42 <billybobjoe7342@gmail.com>
2020-01-09 09:25:43 -0800

    Implemented new foundation method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRedFoundationWall.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+//@Disabled
+@Autonomous(name="New Red Foundation Wall") public class NewRedFoundationWall extends LinearOpMode {
+	/*Initializations*/
+	private Telemetry tele = telemetry;
+	private __Hardware__ h = new __Hardware__(tele, this);
+	private __AutoBase__ a = new __AutoBase__(h, this);
+	private __Foundation__ f = new __Foundation__();
+
+	// Runs when initialized
+	@Override
+	public void runOpMode() {
+		// Initiate hardware
+		try {
+			h.init(hardwareMap);
+		} catch (Exception e) {
+			h.tStatus("Error");
+			h.tErr("HardwareMap", e);
+			sleep(15_000);
+			stop();
+		}
+		h.tStatus("Ready");
+		waitForStart();
+		resetStartTime();
+		try {
+			h.tStatus("Running");
+			/* Instructions - New Red Foundation Wall */
+			a.movL(3,1);
+			a.movB(3,1);
+			a.latch();
+			a.drivePower(1,.5,1,.5);
+			sleep(1500);
+			a.drivePower(0);
+			a.movB(2,1);
+			a.unlatch();
+			a.movL(1.5,1);
+			a.movF(2.5,1);
+			h.tStatus("Done!");
+			/* End */
+		} catch (Exception e) { // Catches exceptions as plain-text
+			h.tStatus("Error");
+			h.tErr("Auto Runtime", e);
+			sleep(15_000);
+			stop();
+		}
+	}
+}
