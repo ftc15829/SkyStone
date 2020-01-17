d97b27b499d3f260d3a0e9a7077090745854643c
EriseZero <erise.zero@gmail.com>
2020-01-17 11:16:33 -0800

    Updated foundation autonomous'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFoundation.java
@@ -29,23 +29,22 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 		try {
 			h.tStatus("Running");
 			/* Instructions - Blue Foundation Mid */
-			h.tStatus("Latching");
-			a.movB(1.0, 1.0);
-			a.movR(9.0, 1.0);
-			a.movB(6.0, 1.0);
-			a.movB(0.8, 0.5);
+			a.movB(1.0,0.9, 0.8);
+			a.movR(11.5,0.9, 3.8);
+			a.movB(5.2,0.9,2.5);
+			a.movB(1.0,0.5, 1.2);
 			a.latch();
 
-			h.tStatus("Unlatching");
-			a.movF(7.0, 1.0);
-			a.movF(1.4, 0.5);
+			a.drivePower(0.3,1.0,0.3,1.0);
+			sleep(3400);
+			a.drivePower(0);
+			a.driveModeSRE();
+
+			a.movB(3.8,1, 1.8);
 			a.unlatch();
-
-			h.tStatus("Line");
-			a.movL(8.0, 1.0);
-			a.movB(5.5, 1.0);
-			a.movL(5.0, 1.0);
-
+			a.movF(1.0,1,.8);
+			a.movL(2.0,1, 1.5);
+			a.movF(9.0,1, 3.5);
 			h.tStatus("Done!");
 			/* End */
 		} catch (Exception e) { // Catches exceptions as plain-text
@@ -83,6 +82,14 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 		try {
 			h.tStatus("Running");
 			/* Instructions - Blue Foundation Wall */
+			{/*
+				|---------+              ^
+				|                        |
+				|\                    0----0
+				|  \                  |ROBO|
+				|    \                0----0
+				+------------------------------
+			*/}
 			a.movB(1.0,0.9, 0.8);
 			a.movR(11.5,0.9, 3.8);
 			a.movB(5.2,0.9,2.5);
@@ -136,20 +143,22 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 		try {
 			h.tStatus("Running");
 			/* Instructions - Red Foundation Mid */
-			a.movB(1.0, 1.0);
-			a.movL(9.0, 1.0);
-			a.movB(6.0, 1.0);
-			a.movB(0.8, 0.5);
-
+			a.movB(1.0, 0.9, 0.8);
+			a.movL(11.5, 0.9, 3.8);
+			a.movB(5.2, 0.9, 2.5);
+			a.movB(1.0, 0.5, 1.2);
 			a.latch();
-			a.movF(7.0, 1.0);
-			a.movF(1.4, 0.5);
+
+			a.drivePower(1.0, 0.3, 1.0, 0.3);
+			sleep(3400);
+			a.drivePower(0);
+			a.driveModeSRE();
+
+			a.movB(3.8, 1, 1.8);
 			a.unlatch();
-
-			a.movR(8.0, 1.0);
-			a.movB(5.5, 1.0);
-			a.movR(5.0, 1.0);
-
+			a.movF(1.0 ,1, 0.8);
+			a.movR(2.0, 1, 1.5);
+			a.movF(9.0, 1, 3.5);
 			h.tStatus("Done!");
 			/* End */
 		} catch (Exception e) { // Catches exceptions as plain-text
